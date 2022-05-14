afb6b3668e7bf0b61ab0b63b6471ff8d41e90fc3
Side-26 <mr72302@gmail>
2022-05-14 12:21:58 +0430

    initializing the project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template lang="">
+<div class="bg-slate-700 text-center ">
+  <p class="text-red-800">
+    here is hesabro project1
+  </p>
+</div>
+</template>
