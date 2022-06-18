042afeff879f4442442f1e134e4e82a40bc9c0ae
Side-26 <mr72302@gmail>
2022-06-18 14:00:53 +0430

    build ui of footer for desktop

--- src/layout/footer/Footer.layout.vue
+++ src/layout/footer/Footer.layout.vue
@@ -1,9 +1,62 @@
 <template lang="">
-    <footer class="py-10 bg-gray-300 px-14">
-        <section>
+    <footer class="py-10 bg-gray-100 px-24">
+        <section class="flex justify-between   mb-5">
+            <div class="font-IranYecan-bold text-sm">
+                <router-link to="/">
+                    <figure class="w-14 h-14">
+                        <img class="w-full h-full" src="../../assets/img/hesabro.logo.svg" alt="hesabro-logo" />
+           
+                    </figure>
+                </router-link>
+                <h5 class=" text-gray-800 font-IranYecan-bold mt-4">
+                    حسابرو، راهکار جامع مدیریت مالی کسب‌ و ‌کار شما
+                </h5>
+                <div class="my-8 ">
+                    آدرس : <span class="text-slate-500">
+                    کرمان ، میدان قرنی ، ساختمان پدر ، طبقه پنجم</span>
+                </div>
+                <div class=""> تلفن : <a class="text-slate-500" href="tel:+989981396467" data-v-461b212e="">09981396467</a></div>
+                <div class="my-8 ">
+                    شبکه های اجتماعی : <figure>
+                        
+                    </figure>
+                </div>
+            </div>
+            <div class="">
+                <div class="font-IranYecan-bold text-lg">
+                    دسترسی ها
+                </div>
+                <div>
+                    <ul class="text-sm text-slate-900">
+                        <li class="my-6"><router-link class="text-slate-800 font-IranYekan-regular" to="/">خانه</router-link></li> 
+                        <li class="my-6"><router-link class="text-slate-800 font-IranYekan-regular" to="/">خدمات و راهکار ها</router-link></li> 
+                        <li class="my-6"><router-link class="text-slate-800 font-IranYekan-regular" to="/tarefeha">تعرفه ها</router-link></li> 
+                        <li class="my-6"><router-link class="text-slate-800 font-IranYekan-regular" to="/">سوالات متداول</router-link></li> 
+                        <li class="my-6"><router-link class="text-slate-800 font-IranYekan-regular" to="/">ارتباط با ما</router-link></li> 
+                    </ul>
+                </div>
+
+            </div>
+            <div class="flex flex-col items-center">
+                <div class="font-IranYecan-bold mb-16">
+                    مجوز های حسابرو
+                </div>
+                <div class="flex">
+                    
+                    <figure class="w-32 h-32 ml-5 cursor-pointer">
+                        <img class="w-full h-full" src="../../assets/img/download.jfif" alt="etemad-logo"/>
+                    </figure>
+                        <figure class="w-32 h-32 cursor-pointer">
+                            <img class="w-full h-full" src="../../assets/img/samandehi.png" alt="samandehi-logo"/>
+                        </figure>
+                </div>
+            </div>
+            
 
         </section>
-        <p class="text-sm font-IranYekan-regular text-slate-500 text-center">تمامی لوگو ها ، عکس ها و سایر محتوا های قرار داده شده با احترام متعلق به گروه فناوری آواپرداز است و امتیاز استفاده از این اطلاعات تنها برای <a href="https://avapardaz.vc/">
+        <p class="text-sm font-IranYekan-regular text-slate-400 text-center">تمامی لوگو ها ، عکس ها و سایر محتوا های قرار داده شده با احترام متعلق به <a target="_blank" class="hover:text-cyan-600 text-cyan-500 transition-all" href="https://avapardaz.vc/">
+        گروه فناوری آواپرداز
+        </a> و امتیاز استفاده از این اطلاعات تنها برای <a target="_blank"  class="hover:text-cyan-600 text-cyan-500 transition-all" href="https://avapardaz.vc/">
         گروه فناوری آواپرداز
         </a> مجاز است</p>
     </footer>
@@ -11,6 +64,8 @@
 <script>
 export default {
     name:"Footer"
+    ,
+    props:[""]
 }
 </script>
 <style lang="">
