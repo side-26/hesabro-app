b73e20e56de8f4bbf3d54d1fdb90af79ab7fc86f
Side-26 <mr72302@gmail>
2022-06-20 09:01:29 +0430

    change some styles in layout

--- src/layout/navBar/NavBar.layout.vue
+++ src/layout/navBar/NavBar.layout.vue
@@ -1,13 +1,13 @@
 <template lang="">
   <nav class="">
-    <section class="sm:static relative container mx-auto py-3 flex">
-      <router-link to="/">
-        <figure class="mr-8 sm:mr-0 sm:ml-10 lg:ml-14 ml-8">
+    <section class="sm:static relative container mx-auto py-3 flex items-center">
+      <router-link to="/" class="flex items-center sm:pr-8">
+        <figure class="mr-8 sm:mr-0 sm:ml-10 lg:ml-14">
             <img src="../../assets/img/hesabro.logo.svg" alt="logo" />
-          
         </figure>
+        <span class="font-IranYecan-bold sm:hidden mr-5  text-xl text-cyan-600 ">حسابرو</span>
       </router-link> 
-      <button @click="handleShow()" class="sm:hidden w-10 text-cyan-600">
+      <button @click="handleShow()" class="sm:hidden ml-10 mr-auto w-10 text-cyan-600">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
@@ -24,11 +24,12 @@
         </svg>
         
       </button>
-      <ul :class="{ 'scale-100': show,'scale-0':!show }" class="sm:bg-inherit bg-white sm:static sm:mt-0 mt-5 font-IranYekan-regular absolute sm:scale-100   transition-all z-30   overflow-hidden top-6 w-full text-center sm:flex-row flex-col flex justify-between sm:w-3/4 md:w-4/6 lg:w-6/12 xl:w-1/2 items-center text-slate-800">
-        <li class="my-5 sm:my-0"><router-link  to="/" class="hover:text-cyan-600   transition-all">خانه</router-link></li>
-        <li class="my-5 sm:my-0"><a href="#" class="hover:text-cyan-600 transition-all ">خدمات و راهکار ها</a></li>
-        <li class="my-5 sm:my-0"><router-link to="/tarefeha" class="active:text-cyan-600 transition-all ">تعرفه ها</router-link></li>
-        <li class="my-5 sm:my-0"><a href="#" class="hover:text-cyan-600   transition-all text-slate-800"> سوالات متداول</a></li><li class="my-5 sm:my-0"><a href="#" class="hover:text-cyan-600  transition-all text-slate-800"> ارتباط با ما</a></li>
+      <ul :class="{ 'scale-100': show,'scale-0':!show }" class="sm:bg-inherit bg-white sm:static sm:mt-0 mt-5 font-IranYekan-regular absolute sm:scale-100   transition-all z-30   overflow-hidden top-8 w-full text-center sm:flex-row flex-col flex justify-between sm:w-3/4 md:w-4/6 lg:w-6/12 xl:w-1/2 items-center text-slate-800">
+        <li class="my-5 sm:my-0"><router-link @click="hanleShow()"  to="/" class="hover:text-cyan-600   transition-all">خانه</router-link></li>
+        <li class="my-5 sm:my-0"><router-link @click="hanleShow()"  to="/" class="hover:text-cyan-600 transition-all ">خدمات و راهکار ها</router-link></li>
+        <li class="my-5 sm:my-0"><router-link @click="hanleShow()" to="/tarefeha" class="hover:text-cyan-600 transition-all ">تعرفه ها</router-link></li>
+        <li class="my-5 sm:my-0"><router-link @click="hanleShow()"  to="/" class="hover:text-cyan-600   transition-all text-slate-800"> سوالات متداول</router-link></li>
+        <li class="my-5 sm:my-0"><router-link @click="hanleShow()"  to="/" class="hover:text-cyan-600  transition-all text-slate-800"> ارتباط با ما</router-link></li>
       </ul>
     </section>
   </nav>
