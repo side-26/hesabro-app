7e9792714f9e70388e632c5c710e516e99d250eb
Side-26 <mr72302@gmail>
2022-06-21 12:49:24 +0430

    make first section of main responsive for all devices

--- src/views/Home.view/Home.view.vue
+++ src/views/Home.view/Home.view.vue
@@ -7,7 +7,7 @@
                     <h1  class="sm:text-4xl text-3xl font-IranYecan-extraBold sm:leading-loose leading-normal">حسابرو <br/> راهکار جامع مدیریت مالی <br/> و کسب کار شما</h1>
                     
                     <!-- <p  class="font-IranYecan-bold  absolute sm:static sm:w-2/3 w-full md:w-2/4 side-26 right-0 -bottom-11  px-4 sm:px-0  bg-white sm:bg-transparent text-justify leading-6 text-gray-600 text-sm sm:py-0 sm:mt-10 py-10 ">سامانه یکپارچه حسابرو ، راهکاری نوین جهت راه اندازی و مدیریت کسب و کار هاست. حسابرو تمامی نیازهای یک کسب و کار را از امور حسابداری و مالی ، خرید و فروش ، انبارگردانی،فروشگاه اینترنتی و دیگر فرآیند ها را در بستری ساده و کم هزینه فراهم می آورد.</p> -->
-                    <button @click="handleNavigate(s)" class="bg-cyan-600 flex justify-between text-sm rounded-lg mt-5 text-white font-IranYekan-regular py-3 px-6">
+                    <button @click="handleNavigate(s)" class="bg-cyan-600 flex justify-between text-sm rounded-lg mt-10 text-white font-IranYekan-regular py-3 px-6">
                             مشاهده تعرفه ها <span class="rotate-90"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                      <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
                      </svg></span>
@@ -18,7 +18,7 @@
             </div>
     </header>
     <main class="">
-        <section class="mx-auto py-4 px-10 items-center">
+        <section class="mx-auto py-4 px-2 md:px-5 lg:px-10 items-center">
             <ImgContainer v-for="item in advantagesData" :key="item.id">
                 <AdvCart  :cardProperty="item"/>
             </ImgContainer>
