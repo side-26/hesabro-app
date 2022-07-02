0d0c7007a69bc5025dbe03bf00c1ef052ebf9915
Side-26 <mr72302@gmail>
2022-07-02 13:59:55 +0430

    fsaf

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import { BASE_URL } from '@/src/config/url.config'
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASEURL,
+  baseURL: import.meta.env.PROD ? 'https://hesabro.com' : import.meta.env.VITE_API_BASEURL ,
   // headers:{
   //     'Access-Control-Allow-Origin':'*'
   // }
+  // api/web
 })
 
 export default api
