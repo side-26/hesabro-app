cd4966240dda388b3151ca8cb59b78cd88359e4d
mortezahatamikia <m.hatamikia93@gmail.com>
2022-07-06 12:00:29 +0430

    WIP change font styles

--- src/views/NotFound/NotFound.vue
+++ src/views/NotFound/NotFound.vue
@@ -1,9 +1,8 @@
 <template lang="">
   <div class="text-center">
     <div class="text-cyan-500 font-IranYecan-extraBold text-5xl mt-52">خطای 404</div>
-    <div class="my-20">دوست عزیز فک کنم گم شدی بزن رو دکمه دوباره مسیر رو 
-      پیدا کنی.
-    </div>
+    <div class="mt-20">صفحه مورد نظر پیدا نشد</div>
+    <div class="my-8">ممکن است آدرس اشتباه وارد شده باشد یا صفحه پاک شده باشد</div>
     <button class="bg-cyan-500 py-4 text-sm font-IranYecan-bold text-white px-6 rounded-3xl" type="button" @click="this.$router.push('/')">صفحه اصلی</button>
   </div>
 </template>
