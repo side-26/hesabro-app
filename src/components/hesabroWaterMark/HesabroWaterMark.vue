3a164054b8de2672ea18b855abc09165ec681958
Side-26 <mr72302@gmail>
2022-07-19 15:55:04 +0430

    build hesabro waterMark

---
+++ src/components/hesabroWaterMark/HesabroWaterMark.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div class="hidden lg:block min-h-screen z-[1] left-[-20px] absolute top-1/2 -translate-x-1/2">
+    <figure class="w-52 h-52   scale-[9]  top-0">
+      <img class="" src="../../../public/img/hesabro-waterMark.svg" alt="Hesabro-waterMark" />
+    </figure>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HesabroWaterMark',
+  setup(props) {},
+}
+</script>
+<style lang="css">
+.fsd {
+  transform: scale(2);
+}
+</style>
