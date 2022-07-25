bb2328ca5372342e847ac2867eb1832998e4be17
Side-26 <mr72302@gmail>
2022-07-25 11:44:13 +0430

    fix : set disabled and type props for Button component

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,11 +1,23 @@
 <template lang="">
   <div>
-    <button  type="Button" class="text-white w-full font-bold py-3 px-4 bg-cyan-500 hover:bg-cyan-600 transition-all rounded-xl"><slot></slot></button>
+    <button :disabled="disabled" :type="type" class="text-white w-full font-bold py-3 disabled:bg-gray-300 disabled:cursor-not-allowed px-4 bg-cyan-500 hover:bg-cyan-600 transition-all rounded-xl"><slot></slot></button>
   </div>
 </template>
 <script>
 export default {
   name: 'Button',
+  props:{
+    type:{
+      type:String,
+      default:"button",
+      required:true
+    },
+    disabled:{
+      type:Boolean,
+      default:false,
+      required:true
+    }
+  },
   setup(props) {
     
   }
