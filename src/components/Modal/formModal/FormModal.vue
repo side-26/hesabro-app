d2741ca170f348e5e97f3acdf3744ea457ec012f
Side-26 <mr72302@gmail>
2022-07-26 13:54:47 +0430

    fea : add a close button for FormModal

--- src/components/Modal/formModal/FormModal.vue
+++ src/components/Modal/formModal/FormModal.vue
@@ -1,8 +1,15 @@
 <template lang="">
-  <div  class="flex z-[200] bg-slate-800 bg-opacity-40 fixed w-full h-full left-0 top-0 items-center justify-center">
-    <div class="p-5 bg-white rounded-xl z-[201] w-full md:w-1/2 lg:w-1/4 2xl:w-1/5 mx-5 sm:mx-8 md:mx-32 lg:mx-0">
-      <div class="font-extrabold">
-        {{ title }}
+  <div @click="handelCloseModal()" class="flex z-[200] bg-slate-800 bg-opacity-40 fixed w-full h-full left-0 top-0 items-center justify-center">
+    <div @click="stopPropagation($event)" class="px-5 py-7 bg-white rounded-xl z-[201] w-full md:w-1/2 lg:w-1/4 2xl:w-1/5 mx-5 sm:mx-8 md:mx-32 lg:mx-0">
+      <div class="font-extrabold flex justify-between items-center">
+        <div>
+          {{ title }}
+        </div>
+        <button @click="handelCloseModal()" type="button">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500 transition-all hover:text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
       </div>
       <slot></slot>
     </div>
@@ -22,8 +29,12 @@ export default {
     const handelCloseModal = () => {
       emit('update:modelValue', false)
     }
+    const stopPropagation = (evt) => {
+      evt.stopPropagation()
+    }
     return {
       handelCloseModal,
+      stopPropagation,
     }
   },
 }
