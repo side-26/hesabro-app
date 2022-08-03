8cd0435a948f728c2560da1b89d8141049391616
side-26e <mr72302@gamil.com>
2022-08-03 11:46:50 +0430

    fix : install all packages related to jest and unitTesting and write first test for this project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        {{msg}}
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        msg:String
+    }
+}
+</script>
+<style lang="">
+    
+</style>
