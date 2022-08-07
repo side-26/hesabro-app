92aa40e06afbe43bb6bc8855ca305ed4149e9f5b
Side-26 <mr72302@gmail>
2022-08-07 21:18:22 +0430

    style : set new styles for TotalPriceContainer

--- src/components/TotalPriceContainer/TotalPriceContainer.vue
+++ src/components/TotalPriceContainer/TotalPriceContainer.vue
@@ -1,5 +1,5 @@
 <template lang="">
-  <div class="md:border-t-2 md:block flex justify-between w-full px-5 pb-1 md:pt-4 md:border-gray-200 md:mt-5 mt-3">
+  <div class="md:border-t-2 md:block flex justify-between w-full lg:px-5 pb-1 md:pt-4 md:border-gray-200 md:mt-5 mt-3">
     <span class="text-sm text-gray-700 font-bold">{{ title }} :</span>
     <span class="font-bold text-black side-26"
       >{{ toSepratedFarsiNo(totalPrice) }}
