e2e3cbd6436f0f383ae0dcffd5e1c3606333ef53
side-26e <mr72302@gamil.com>
2022-08-10 12:14:45 +0430

    fix : set new function for show a value in farsi

--- src/components/UserForm/Input/Input.vue
+++ src/components/UserForm/Input/Input.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="flex-col flex mb-3">
     <label class="font-medium text-xs mb-3 text-gray-700">{{ title }}</label>
-    <Field class="bg-gray-200 text-sm text-gray-800 font-bold h-12 pr-1 rounded-xl placeholder:font-bold placeholder:text-xs placeholder:pr-1" :name="name" :type="type"  :placeholder="placeHolder" />
+    <Field @change="handleShowFarsi($event)" class="bg-gray-200 text-sm text-gray-800 font-bold h-12 pr-1 rounded-xl placeholder:font-bold placeholder:text-xs placeholder:pr-1" :name="name" :type="type" :placeholder="placeHolder" />
     <ErrorMessage class="text-xs font-bold text-red-600 mt-2" :name="name" />
   </div>
 </template>
 <script>
 import { Field, ErrorMessage } from 'vee-validate'
+import { toFarsiNumber } from '@/utilities/ConvertToPersian'
 
 export default {
   components: {
@@ -27,6 +28,15 @@ export default {
       type: String,
       required: true,
     },
+  },
+  setup(props) {
+    const handleShowFarsi = (evt) => {
+      evt.target.value = toFarsiNumber(evt.target.value)
+    }
+    return {
+      toFarsiNumber,
+      handleShowFarsi,
+    }
   },
 }
 </script>
